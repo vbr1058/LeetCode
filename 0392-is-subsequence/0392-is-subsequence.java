44d6bdaf5d874f4acac77efbc5e841ef3ce74759
Vinay B R <43924856+vbr1058@users.noreply.github.com>
2025-02-23 23:42:30 +0530

    Time: 2 ms (65.34%), Space: 41.5 MB (71.86%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int count=0;
+        int i=0;
+        int j=0;
+        while (i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                count++;
+                i++;
+                j++;
+            }
+            else {
+                j++;
+            }
+        }
+        if (count==s.length()){
+            return true;
+        }
+        return false;
+    }
+}
